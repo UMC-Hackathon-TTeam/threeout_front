a59a46262b9692a13680deca225907c4e16d74c5
hanjeonghyun <limithyunii@gmail.com>
2024-01-03 00:52:42 +0900

    [FIX] footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
   return (
     <Navwrapper>
       <NavLink
-        to='/'
-        className={location.pathname.startsWith === "/main" ? "active" : ""}
+        to='/main'
+        className={location.pathname.startsWith("/main") ? "active" : ""}
       >
         <IconWrapper
           src={Main}
@@ -21,7 +21,7 @@ const Footer = () => {
       </NavLink>
       <NavLink
         to='/Danger'
-        className={location.pathname.startsWith === "/Danger/" ? "active" : ""}
+        className={location.pathname.startsWith("/Danger") ? "active" : ""}
       >
         <IconWrapper
           src={danger}
@@ -30,7 +30,7 @@ const Footer = () => {
       </NavLink>
       <NavLink
         to='/Ranking'
-        className={location.pathname.startsWith === "/Ranking/" ? "active" : ""}
+        className={location.pathname.startsWith("/Ranking") ? "active" : ""}
       >
         <IconWrapper
           src={Ranking}
@@ -48,19 +48,25 @@ const Navwrapper = styled.div`
   justify-content: space-between;
   position: fixed;
   bottom: 30px;
-  left: 5px;
+  left: 0px;
+  right: 0px;
   max-width: 530px;
   background: #fff;
+  border-top: 0.4px solid #545f71;
 `;
 
 const IconWrapper = styled.img`
   display: flex;
   margin: 20px 60px 0px 30px;
   text-decoration: none;
-  filter: ${({ active }) =>
-    active
-      ? "invert(84%) sepia(46%) saturate(291%) hue-rotate(121deg) brightness(91%) contrast(94%)"
-      : "invert(43%) sepia(1%) saturate(0%) hue-rotate(137deg) brightness(91%) contrast(90%)"};
+  filter: invert(43%) sepia(1%) saturate(0%) hue-rotate(137deg) brightness(91%)
+    contrast(90%);
+
+  &.active {
+    filter: invert(84%) sepia(46%) saturate(291%) hue-rotate(121deg)
+      brightness(91%) contrast(94%);
+  }
+
   width: 50px;
   height: 50px;
 `;
